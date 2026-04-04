fdae5d9115f79a7680fcc7c415840610a5dad946
Osman Mohamed <osman.mohamed0213@gmail.com>
2026-04-03 19:49:54 -0500

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+#   ["Breaking", "Bad"] -> "8|Breaking3|Bad"
+    def encode(self, strs: List[str]) -> str:
+        return ''.join(f"{len(s)}|{s}" for s in strs)
+
+#   "8|Breaking3|Bad" -> ["Breaking", "Bad"] 
+    def decode(self, s: str) -> List[str]:
+        res = []
+        num = ""
+        i = 0
+        while i < len(s):
+            c = s[i]
+            if c != "|":
+                num += c
+                i += 1
+                continue
+            num = int(num)
+            word = s[i + 1: i + num + 1]
+            res.append(word)
+            i += num + 1
+            num = ""
+        return res
