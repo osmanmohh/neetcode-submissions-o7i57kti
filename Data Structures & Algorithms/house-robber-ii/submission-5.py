f0645ea6144292f87235ace22eee003ba6f79b0c
Osman Mohamed <osman.mohamed0213@gmail.com>
2026-04-04 22:14:45 -0500

    Add: house-robber-ii - submission-5

---
+++ Data Structures & Algorithms/house-robber-ii/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        def robLinear(nums):
+            memo = {}
+            def f(i):
+                if i in memo:
+                    return memo[i]
+                if i >= len(nums):
+                    return 0
+                memo[i] = max(nums[i] + f(i + 2), f(i + 1))
+                return memo[i]
+            return f(0)
+        return max(robLinear(nums[:-1]), robLinear(nums[1:]))
