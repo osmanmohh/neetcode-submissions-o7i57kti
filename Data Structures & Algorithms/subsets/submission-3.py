b0ac4e4dbba56ba5a638676a244e9742138787b1
Osman Mohamed <osman.mohamed0213@gmail.com>
2026-04-08 11:01:38 -0500

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        curr = []
+        def dfs(i):
+            if i >= len(nums):
+                res.append(curr[:])
+                return
+            curr.append(nums[i])
+            dfs(i + 1)
+            curr.pop()
+            dfs(i + 1)
+            
+            return res
+        
+        return dfs(0)
