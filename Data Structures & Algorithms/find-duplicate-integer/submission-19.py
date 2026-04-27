9786f620cb3f6bc2a0cade92b354d32ed8298eae
Osman Mohamed <osman.mohamed0213@gmail.com>
2026-04-26 22:21:02 -0500

    Add: find-duplicate-integer - submission-19

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-19.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            num = nums[i]
+            idx = abs(num) - 1
+            if nums[idx] < 0:
+                return idx + 1
+            nums[idx] = -abs(nums[idx])
+        
